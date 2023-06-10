f68f48967751c7f28638c399cd6996537abc4b60
hkshabiya <hkshabiya@gmail.com>
2023-06-10 20:51:53 +0530

    Modified routers

--- src/components/CardPage.js
+++ src/components/CardPage.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import CardList from './CardList';
 const CardPage = () => {
   return (
     <Router>
       <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">Volopay</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <Link className="navbar-brand" to="/">Volopay</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item">
         <Link to="/">All</Link>
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
         <Link to="/your">Your</Link>
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
         <Link to="/blocked">Blocked</Link>
       </li>
     </ul>
@@ -26,9 +26,10 @@ const CardPage = () => {
 </nav>
 
         <Routes>
+          <Route exact path="/" element={<CardList type='all'/>} />
           <Route path="/your" element={<CardList type='your'/>} />
-          <Route path="/" element={<CardList type='all'/>} />
           <Route path="/blocked" element={<CardList type='blocked'/>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
