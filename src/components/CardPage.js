3322a596d626d99f6ba555945bc04357671eb16a
hkshabiya <hkshabiya@gmail.com>
2023-06-10 18:31:21 +0530

    Mod nav

--- src/components/CardPage.js
+++ src/components/CardPage.js
@@ -5,23 +5,29 @@ const CardPage = () => {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/your">Your</Link>
-            </li>
-            <li>
-              <Link to="/all">All</Link>
-            </li>
-            <li>
-              <Link to="/blocked">Blocked</Link>
-            </li>
-          </ul>
-        </nav>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/">Volopay</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <Link to="/">All</Link>
+      </li>
+      <li class="nav-item">
+        <Link to="/your">Your</Link>
+      </li>
+      <li class="nav-item">
+        <Link to="/blocked">Blocked</Link>
+      </li>
+    </ul>
+  </div>
+</nav>
 
         <Routes>
           <Route path="/your" element={<CardList type='your'/>} />
-          <Route path="/all" element={<CardList type='all'/>} />
+          <Route path="/" element={<CardList type='all'/>} />
           <Route path="/blocked" element={<CardList type='blocked'/>} />
         </Routes>
       </div>
